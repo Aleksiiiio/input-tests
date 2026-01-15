2dbdbb9803549f18b9c15b79aa8b061c718da646
Aleksiiiio <galuolek@gmail.com>
2026-01-15 15:27:38 +0100

    images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [gooningColor, setGooningColor] = useState(false);
+  const [freakyColor, setFreakyColor] = useState(false);
+
+  function imageClickHandle(e){
+    if(e.target.id == "gooning") {
+      setGooningColor(!gooningColor);
+      setFreakyColor(false);
+    }
+    if(e.target.id == "freaky") {
+      setFreakyColor(!freakyColor)
+      setGooningColor(false);
+    };
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='d-flex flex-column'>
+          <div  className={`d-flex align-items-center justify-content-center ${gooningColor ? "bg-primary" : ""} ${freakyColor ? "bg-secondary" : ""}`} id='images'>
+            <img src='gooning.png' alt='gooning' className='w-25 img-fluid' id='gooning' onClick={imageClickHandle}/>
+            <img src='freaky.gif' alt='freaky' className='w-25 img-fluid' id='freaky' onClick={imageClickHandle}/>
+          </div>
+      </div>
     </div>
   );
 }
